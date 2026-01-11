804d9ef4ebd25db75306456038a925a930fada92
Johannes Medagbe <johanmedagbe@gmail.com>
2026-01-11 06:07:42 +0300

    Draft code

---
+++ src/select.jl
@@ -0,0 +1,89 @@
+"""
+Main script of the program
+To run: julia select.jl
+"""
+
+using DataFrames
+using Utils
+import CSV
+
+INPUT_PATH = ENV["INPUT_PATH"]
+MODEL_NAME = ENV["MODEL_NAME"]
+NUM_QUERIES = ENV["NUM_QUERY"]
+TOP_RESULTS = ENV["TOP_RESULTS"]
+OUTPUT_PATH = ENV["OUTPUT_PATH"]
+
+"""
+Represents a paper selector
+"""
+struct Selector
+    input_path:: String
+    output_path: String
+    model_name:: String
+    num_queries:: Int8
+    top_results: Int8
+end
+
+
+"""
+Function that extracts embeddings for documents and queries:
+"""
+function get_embeddings(selector::Selector)
+    # Load model
+    sentence_transformers = pyimport("sentence_transformers")
+    model = sentence_transformers.SentenceTransformer(selector.model_name, trust_remote_code=true)
+
+    collection, docs = preprocess(selector.input_path)
+    doc_embeds = process_embed(docs, model)
+
+    println("Enter $(selector.num_queries) instructions one at a time:")
+    queries = Corpus([StringDocument(readline(), TextAnalysis.DocumentMetadata(Languages.English(), "Query $n")) for n in 1:selector.num_queries])
+    query_embs = process_embed(queries, model)
+
+    return collection, queries, query_embeds, doc_embeds
+end
+
+
+"""
+Function that evaluates the relevance of a document for each query based on embeddings and writes the results
+"""
+function evaluate_relevance(
+     selector::Selector,
+     collection::OrderedCollections.OrderedDict,
+     queries::Corpus,
+     query_embeds::PyArray,
+     doc_embeds::PyArray)
+
+    queries = [text(query) for query in documents(queries)]
+    dfs = DataFrame[]
+    
+    for query, query_embed in zip(queries, query_embeds)
+    	cos = [compute_cosm(query_embed, view(doc_embeddings, row, :)) for row in 1:size(doc_embeds, 1)] # compute similarity row by row along first dimension
+	sorted = sortperm(cos, rev=true)[1:selector.top_results] # get indices of best documents
+	scores = cos[sorted]
+	
+	# Extract corresponding titles
+	keys = collect(keys(ori))
+	titles = [keys[el] for el in sorted]
+
+	# Organize results
+	df = DataFrame(
+	    "queries" = [query for 1:selector.top_results],
+	    "titles" = titles,
+	    "scores" = scores)
+	push!(dfs, df)	
+    end
+    CSV.write(selector.output_path, dfs)
+end
+
+
+# Main
+selector = Selector(
+    input_path=INPUT_PATH,
+    output_path=OUTPUT_PATH,
+    model_name=MODEL_NAME,
+    num_queries=NUM_QUERIES,
+    top_results=TOP_RRSULTS)
+
+collection, queries, query_embeds, doc_embeds = get_embeddings(selector)
+evaluate_relevance(selector, collection, queries, query_embeds, doc_embeds)
---
+++ src/utils.jl
@@ -0,0 +1,84 @@
+"""Module with helper functions"""
+
+module Utils
+
+# Import required modules
+using PDFI
+using TextAnalysis
+using LinearAlgebra
+using Languages
+using PyCall
+using SparseArrays
+
+"""
+Function that preprocess pdf documents:
+- Takes directory containing documents in pdf format
+- Parses papers and extract title and body
+- Returns dictionary of papers with title as key and body as value
+"""
+function preprocess(dir_path::String)
+    collection = Dict{String, String}()
+    files = readdir(dir_path)
+    title = ""
+    for file in files
+    	doc = pdDocOpen(file)
+	number_page = pdDocGetPageCount(doc)
+	for page in number_page
+	    ref = pdDocGetPage(doc, page)
+	    io = IOBuffer()
+	    pdPageExtractText(io, ref)
+	    content = String(take!(io))
+	    indices = findfirst("introduction", lowercase(content))
+	    if indices isa UnitRange
+	        global title
+	        title = strip(content[begin:indices[begin-1]]) # take care of title cleaning separately
+	        other = content[indices[begin]:]
+	    else
+		other = content
+	    end
+	    if length(title) != 0
+	        if title not in collections
+	       	    collection[title] = other
+		else
+		    collection[title] *= other
+		end
+	    end
+
+    collection = sort(collection) # sort by key to facilitate traceback
+    corpus = Corpus([StringDocument(collection[title], TextAnalysis.DocumentMetadata(Languages.English(), title)) for title in collection])
+
+    return collection, corpus
+end
+
+
+"""
+Function that completes processing of documents and embeds their contents:
+- Takes a Corpus
+- Process in-place contents of StringDocument(s) in Corpus
+- Embeds StringDocument contents using specified model
+- Returns embeddings of cleaned StringDocument(s)
+"""
+function process_embed(
+    docs::Corpus,
+    model::PyObject,
+    normalize=true)
+    prepare!(docs, strip_punctuation | strip_numbers | strip_case | strip_whitespace)
+    texts = [text(doc) for doc in documents(docs)]
+    embeddings = model.encode(texts, normalize_embeddings=normalize)
+    return PyArray(embeddings)
+end
+
+
+"""
+Function that computes cosine similarity between two embedding vectors:
+- Takes embedding vectors a and b
+- Computes and returns cosine similarity measure
+"""
+function compute_cosm(a::AbstractVector, b::AbstractVector)
+    num = dot(a, b)
+    den = norm(a) * norm(b)
+    return den > 0 ? num / den : 0f0
+end
+
+
+end
